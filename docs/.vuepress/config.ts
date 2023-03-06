ed538b2568dce49d67f387a0f92043d95869a40b
legalgeek@vip.cn <legalgeek@vip.cn>
2023-03-06 23:35:36 +0800

    202303062335

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -53,6 +53,30 @@ export default defineUserConfig({
           children: ["/criminal-law/zongze.md", "/criminal-law/fenze.md"],
         },
       ],
+    },
+    locales:{
+      '/': {
+        
+        // page meta
+        editLinkText: '在 GitHub 上编辑此页',
+        lastUpdatedText: '上次更新',
+        contributorsText: '贡献者',
+        // custom containers
+        tip: '提示',
+        warning: '注意',
+        danger: '警告',
+        // 404 page
+        notFound: [
+          '这里什么都没有',
+          '这是一个 404 页面',
+          '看起来我们进入了错误的链接',
+        ],
+        backToHome: '返回首页',
+        // a11y
+        openInNewWindow: '在新窗口打开',
+        toggleColorMode: '切换颜色模式',
+        toggleSidebar: '切换侧边栏',
+      }
     }
     
   }),
